827d0eec37cdf0ef6a184046bb38ea058161301d
Karim Naaji <karim.naaji@gmail.com>
2014-05-14 13:01:08 +0200

    merge camera branch

--- particle-system/src/main.cpp
+++ particle-system/src/main.cpp
@@ -1,15 +1,7 @@
 #include <QApplication>
 #include "app.h"
-#include "camera.h"
 
 int main(int argc, char *argv[]) {
-    Vec3 position(0,10,0);
-    Vec3 xAxis(1, 0, 0);
-    Quaternion q(M_PI_2, xAxis);
-    Camera c(position, q, 4/3, 90, 1, 10);
-
-    cout << c.getProjectionMatrix() << endl;
-
     QApplication app(argc, argv);
     App win;
     win.show();
