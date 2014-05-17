8935d8cf755c169aa75265fb428b9938e84e7ce3
Karim Naaji <karim.naaji@gmail.com>
2014-05-17 13:08:05 +0200

    change some default parameters in camera

--- particle-system/src/camera.h
+++ particle-system/src/camera.h
@@ -15,27 +15,27 @@ class Camera : public SceneNode
                Quaternion orientation,
                float aspectRatio,
                float fovAngle = 50.0f,
-               float nearPlane = 0.01f,
+               float nearPlane = 1.0f,
                float farPlane = 1000.0f,
                ProjectionType type = Perspective);
-		~Camera();
-		Matrix4 getViewMatrix();
-		Matrix4 getProjectionMatrix();
-		void setAspectRatio(float ratio);
-		void setFOV(float angle);
+        ~Camera();
+        Matrix4 getViewMatrix();
+        Matrix4 getProjectionMatrix();
+        void setAspectRatio(float ratio);
+        void setFOV(float angle);
         void setPlanes(float near, float far);
         Vec3 up();
         Vec3 right();
         Vec3 forward();
 
-	protected:
-		void buildViewMatrix();
-		void buildProjectionMatrix();
+    protected:
+        void buildViewMatrix();
+        void buildProjectionMatrix();
 
-	private:
+    private:
         ProjectionType projectionType;
         Matrix4 viewMatrix, projectionMatrix;
-		float aspectRatio, fovAngle, farPlane, nearPlane;
+        float aspectRatio, fovAngle, farPlane, nearPlane;
 };
 
 #endif // CAMERA_H
